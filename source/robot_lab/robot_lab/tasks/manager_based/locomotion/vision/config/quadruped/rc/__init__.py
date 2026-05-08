0cc45ed91b422934b203aaf9ed7862822b193f1f
RCororot <13503094424@139.com>
2026-05-08 13:08:38 +0800

    ini

---
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/vision/config/quadruped/rc/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2024-2025 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="RobotLab-Isaac-Vision-Flat-RC-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:RCVisionFlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:RCVisionFlatPPORunnerCfg",
+        "cusrl_cfg_entry_point": f"{agents.__name__}.cusrl_ppo_cfg:RCVisionFlatTrainerCfg",
+    },
+)
+
+gym.register(
+    id="RobotLab-Isaac-Vision-Rough-RC-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:RCVisionRoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:RCVisionRoughPPORunnerCfg",
+        "cusrl_cfg_entry_point": f"{agents.__name__}.cusrl_ppo_cfg:RCVisionRoughTrainerCfg",
+    },
+)

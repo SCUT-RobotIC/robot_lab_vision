1bed217a07cd57c6143e0ab37720a51dde5b5ff1
RCororot <13503094424@139.com>
2026-05-14 17:24:46 +0800

    补充

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/vision/config/quadruped/rc/__init__.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/vision/config/quadruped/rc/__init__.py
@@ -19,23 +19,13 @@ gym.register(
     },
 )
 
-gym.register(
-    id="RobotLab-Isaac-Vision-Student-RC-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.student_env_cfg:RCVisionStudentEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_distillation_cfg:RCStudentDistillationRunnerCfg",
-    },
-)
-
 
 gym.register(
     id="RobotLab-Isaac-Vision-Teacher-Student-RC-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.TS_env_cfg:AnymalDFlatEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.TS_env_cfg:RCVisionTeacherEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:RCTeacherPPORunnerCfg",
         "rsl_rl_distillation_cfg_entry_point": (
             f"{agents.__name__}.rsl_rl_distillation_cfg:RCStudentDistillationRunnerCfg"

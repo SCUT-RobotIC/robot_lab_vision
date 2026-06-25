79aa75642deb4c23b8d083304742d5f788702b4c
wang jinghua <147704638+GZ89mid@users.noreply.github.com>
2026-06-25 16:33:37 +0800

    适配狗脚名

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/quadruped/rc/low_bar_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/quadruped/rc/low_bar_env_cfg.py
@@ -23,7 +23,7 @@ class RCLowBarEnvCfg(RCRoughEnvCfg):
     """
 
     base_link_name = "base_link"
-    foot_link_name = ".*_feet_joint"
+    foot_link_name = ".*_feet_link"
 
     low_bar_x = 1.0
     low_bar_height = 0.30
